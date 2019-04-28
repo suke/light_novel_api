0f48762928ae94de916eeb3fd992e7037a21947c
suke <suke083.tech@gmail.com>
2019-04-28 19:16:43 +0900

    fix novels seed

--- db/seeds/novels.rb
+++ db/seeds/novels.rb
@@ -8,6 +8,9 @@ CSV.foreach('db/csv/novels.csv', headers: true) do |row|
     book_size: row['book_size'],
     isbn: row['isbn'],
     price: row['price'],
-    release_date: row['release_date']
+    release_date: row['release_date'],
+    author_id: row['author_id'],
+    illustrator_id: row['illustrator_id'],
+    publisher_id: row['publisher_id']
   )
 end
